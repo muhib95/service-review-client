6f17cc3883f370a6e276c94f5c2a3ec788408d04
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-11-07 21:34:00 +0600

    route setup

---
+++ src/Routes/Routes.js
@@ -0,0 +1,18 @@
+import Main from "../Main/Main";
+import Home from "../Pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+
+])
