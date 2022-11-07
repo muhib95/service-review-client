6eb29381bc3a9b67d87a9b60fe84c082f2a29494
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-11-08 00:24:24 +0600

    home services add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import HomeService from '../HomeService/HomeService';
 
 const Home = () => {
+    const services=useLoaderData();
+    
     return (
         <div>
             <div className="hero  bg-base-200">
@@ -13,6 +17,16 @@ const Home = () => {
     </div>
   </div>
 </div>
+<div className='grid grid-cols-1 md:grid-cols-3 gap-3 my-4'>
+  
+    {
+services.map(service=><HomeService key={service._id} service={service}></HomeService>)
+    }
+</div>
+<div className='w-full flex justify-center'>
+<button className="btn btn-primary  ">See All</button>
+</div>
+
         </div>
     );
 };
