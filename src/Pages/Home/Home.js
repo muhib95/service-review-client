d28699ca0447cab795513801aa082902dd0bbf4d
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-11-09 22:45:28 +0600

    footer logo some change

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -45,18 +45,18 @@ services.map(service=><HomeService key={service._id} service={service}></HomeSer
 </div>
 <div>
 <section className="p-6 text-gray-100">
-	<form novalidate="" className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow bg-gray-900 ng-untouched ng-pristine ng-valid">
+	<form className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow bg-gray-900 ng-untouched ng-pristine ng-valid">
 		<h2 className="w-full text-3xl font-bold leading-tight">Contact us</h2>
 		<div>
-			<label for="name" className="block mb-1 ml-1">Name</label>
+			<label htmlFor="name" className="block mb-1 ml-1">Name</label>
 			<input id="name" type="text" placeholder="Your name" required="" className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 bg-gray-800" />
 		</div>
 		<div>
-			<label for="email" className="block mb-1 ml-1">Email</label>
+			<label htmlFor="email" className="block mb-1 ml-1">Email</label>
 			<input id="email" type="email" placeholder="Your email" required="" className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 bg-gray-800" />
 		</div>
 		<div>
-			<label for="message" className="block mb-1 ml-1">Message</label>
+			<label htmlFor="message" className="block mb-1 ml-1">Message</label>
 			<textarea id="message" type="text" placeholder="Message..." className="block w-full p-2 rounded autoexpand focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 bg-gray-800"></textarea>
 		</div>
 		<div>
