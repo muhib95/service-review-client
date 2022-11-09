17f376dad139740f415d428b8290bcca9876218a
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-11-09 21:34:06 +0600

    responsive header

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useTitle from '../../hook/useTitle';
 import HomeService from '../HomeService/HomeService';
+import img from '../../asset/home.png'
 
 const Home = () => {
     // const services=useLoaderData();
@@ -20,7 +21,7 @@ const Home = () => {
         <div>
             <div className="hero  bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
-    <img src='https://placeimg.com/260/400/arch' alt='' className="max-w-sm rounded-lg shadow-2xl" />
+    <img src={img} alt='' className="max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../../AuthContext/AuthContext';
+import logo from '../../../asset/logo.jpg'
 
 const Header = () => {
     const {user,logOut}=useContext(UserContext);
@@ -23,22 +24,25 @@ console.log(user?.email);
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link>Item 1</Link></li>
-        <li tabIndex={0}>
-          <Link className="justify-between">
-            Parent
-            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-          </Link>
-          <ul className="p-2">
-            <li><Link>Submenu 1</Link></li>
-            <li><Link>Submenu 2</Link></li>
-          </ul>
-        </li>
-        <li><Link>Item 3</Link></li>
+        <li><Link to='/'>Home</Link></li>
+      {
+      user?.email?
+        <>
+         <li><Link to='/myreview'>My Review</Link></li>
+      <li><Link to='/addservice'>Add service</Link></li>
+      <li><button onClick={handleLogOut}>Log out</button></li>
+        </>
+        :
+        <>
+              <li><Link to='/login'>Login</Link></li>
+    <li><Link to='/register'>Register</Link></li>
+        </>
+      }
+      <li><Link to='/blogs'>Blogs</Link></li>
       </ul>
     </div>
     <Link  to='/' className=" normal-case text-xl">
-      <img src='' alt="" />
+      <img className='w-16 hidden md:inline' src={logo} alt="" />
     </Link>
   </div>
   <div className="navbar-center hidden lg:flex">
