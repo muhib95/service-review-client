b1985c75998e574a38dd1ff853697ef0071ae1fb
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-11-09 12:08:35 +0600

    my review part data get

--- src/Pages/MyReview/MyReview.js
+++ src/Pages/MyReview/MyReview.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../AuthContext/AuthContext';
+import MyReviewRows from '../MyReviewRows/MyReviewRows';
 
 const MyReview = () => {
+    const {user}=useContext(UserContext);
+    const email=user?.email||'undefined';
+    const [myReviews,setReviews]=useState([]);
+    useEffect(()=>{
+        fetch(`http://localhost:5000/myreviews?email=${email}`)
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+    },[email])
     return (
         <div>
-            Revi
+            <h1>All my reviews</h1>
+            <h2>{myReviews.length}</h2>
+            {
+            myReviews.map(myReview=><MyReviewRows key={myReview._id} myReview={myReview}></MyReviewRows>)    
+            }
         </div>
     );
 };
---
+++ src/Pages/MyReviewRows/MyReviewRows.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyReviewRows = ({myReview}) => {
+    console.log(myReview);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyReviewRows;
